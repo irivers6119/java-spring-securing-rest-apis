22d0af143712435cf3ddd5845478be063c7f8787
Ignacio Rivers <ignaciorivers@coxautoinc.com>
2022-02-13 20:27:19 -0500

    Completed module

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserAuthority.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserAuthority.java
@@ -1,4 +1,39 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity(name="authorities")
 public class UserAuthority {
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Id
+    UUID id;
+    @Column
+    String authority;
+
+
+    @JoinColumn(name="username", referencedColumnName = "username")
+    @ManyToOne
+    User user;
+   UserAuthority() {}
+    public UserAuthority(User user, String authority) {
+       this.id = UUID.randomUUID();
+       this.user = user;
+       this.authority = authority;
+    }
 }
