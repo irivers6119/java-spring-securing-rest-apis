922786ec274f333c690d094df1907718f82198b5
Ignacio Rivers <ignaciorivers@coxautoinc.com>
2022-02-15 23:19:21 -0500

    enabled cors

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
@@ -23,7 +23,7 @@ public class ResolutionController {
 		this.users = users;
 	}
 
-	@CrossOrigin //(maxAge=0) if locally verifying
+	@CrossOrigin (allowCredentials = "true") // if locally verifying
 	@GetMapping("/resolutions")
 	@PreAuthorize("hasAuthority('resolution:read')")
 	@PostFilter("@post.filter(#root)")
